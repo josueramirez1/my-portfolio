0b538b9d3f1b24c4f9e5922fa6370bee43d5ba27
josueramirez1 <josue67ramirez@gmail.com>
2025-03-22 17:27:29 -0700

    adding parcell

--- dist/my-portfolio.07ffef23.js
+++
@@ -1,7 +0,0 @@
-const hamburgerBtn = document.querySelector(".hamburger");
-const hamburgerList = document.querySelector(".hamburger-list-container");
-hamburgerBtn.addEventListener("click", (e)=>{
-    hamburgerList.classList.toggle("hidden");
-});
-
-//# sourceMappingURL=my-portfolio.07ffef23.js.map
