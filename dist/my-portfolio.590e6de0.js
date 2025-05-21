e9bea504d4d0fa5114d34c49e0560972a0a4b759
Josue Ramirez <84062963+josueramirez1@users.noreply.github.com>
2025-05-21 00:02:39 -0700

    Delete dist directory

--- dist/my-portfolio.590e6de0.js
+++
@@ -1,2 +0,0 @@
-let hamburgerBtn=document.querySelector(".hamburger"),hamburgerList=document.querySelector(".hamburger-list-container");hamburgerBtn.addEventListener("click",e=>{hamburgerList.classList.toggle("hidden")});
-//# sourceMappingURL=my-portfolio.590e6de0.js.map
